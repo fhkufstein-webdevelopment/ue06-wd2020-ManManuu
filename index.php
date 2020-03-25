b4d56cbce7ca43685208a8ece5f564eb3aea2f93
ManManuu <61968310+ManManuu@users.noreply.github.com>
2020-03-25 23:29:10 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,3 +1,37 @@
 <?php
 
-//@TODO insert your code here
+require_once('includes/classes/Database.php');
+
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'manuelsdb');
+define('DB_USER', 'Manuel');
+define('DB_PASS', 'test1234');
+
+$db = new Database();
+
+$cryptedPassword = password_hash('testpassword', PASSWORD_BCRYPT);
+$username = "test";
+
+$cryptedPassword = $db->escapeString($cryptedPassword);
+$username = $db->escapeString($username);
+
+$sql = "SELECT * FROM user WHERE name='".$username."'";
+$result = $db->query($sql);
+if($db->numRows($result) > 0)
+{
+    $row = $db->fetchAssoc($result);
+    if(password_verify("passwort1234", $row['password']))
+    {
+        echo "Der Nutzer ".$username." mit der ID ".$row['id']." hat";
+        echo " das Passwort testpassword";
+    }
+    else
+    {
+        echo "Nutzer gefunden aber falsches Passwort!";
+    }
+}
+else
+{
+    echo "Keinen Nutzer gefunden";
+}
+
